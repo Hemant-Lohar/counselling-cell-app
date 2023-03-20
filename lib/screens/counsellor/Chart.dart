5f8106cd6648687d840f04e444deda33da0765dd
Hemant-Lohar <hemantlohar585@gmail.com>
2023-03-20 10:11:22 +0530

    Chart file created

---
+++ lib/screens/counsellor/Chart.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Chart extends StatefulWidget {
+  const Chart({super.key});
+
+  @override
+  State<Chart> createState() => _ChartState();
+}
+
+class _ChartState extends State<Chart> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
